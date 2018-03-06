9e94fd4af17014f185cce602fcd3727f8d3ff0d1
Jared Vicentillo <jared.vicentillo@gmail.com>
2018-03-06 22:16:48 +0800

    Add create student.

--- acs/views/student.py
+++ acs/views/student.py
@@ -1,6 +1,7 @@
 from acs.models import Profile
 from acs.forms.student import StudentForm
 
+from django.core.urlresolvers import reverse_lazy
 from django.views import generic
 
 
@@ -12,3 +13,4 @@ class CreateView(generic.CreateView):
     template_name = 'acs/student/create.html'
     model = Profile
     form_class = StudentForm
+    success_url = reverse_lazy('create-student')
