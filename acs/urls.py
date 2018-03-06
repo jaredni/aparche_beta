7bef03a8ea7f7d79932d806af4a055d4a800edf9
Jared Vicentillo <jared.vicentillo@gmail.com>
2018-03-06 23:03:35 +0800

    Change create student url.

--- acs/urls.py
+++ acs/urls.py
@@ -4,6 +4,7 @@ from . import views
 from .views import student
 
 urlpatterns = [
-    url(r'^create/$', student.CreateView.as_view(), name='create-student'),
+    url(r'^student/create/$', student.CreateView.as_view(),
+        name='create-student'),
     url(r'^student/list/$', student.ListView.as_view(), name='list-student'),
 ]
